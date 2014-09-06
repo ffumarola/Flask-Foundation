c7ad75899bf69fef67bcc75b3572e056315412ec
Rob Harrigan <harrigan.rob@gmail.com>
2014-09-05 22:52:25 -0400

    Add in the ActiveModel Mixin

--- appname/models/user.py
+++ appname/models/user.py
@@ -1,8 +1,8 @@
-from .db import db
+from .db import db, ActiveModel
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from flask.ext.bcrypt import check_password_hash, generate_password_hash
 
-class User(db.Model, UserMixin):
+class User(db.Model, ActiveModel, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     password = db.Column(db.String)
